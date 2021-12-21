301da1df20c9a32646c4b2aeceb5dbf7670c61ff
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-12-21 11:40:32 +0300

    Добавьте файлы проекта.

---
+++ Ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] cNum = new char[] { '1', ' ', '2', ' ', '3', ' ', '4' };
+            string sNum = "1 2 3 4";
+            int counter = 0;
+
+            for (int i = 0; i < cNum.Length; i++)
+            {
+                if (Char.IsNumber(cNum[i]))
+                {
+                    int digit = int.Parse(cNum[i].ToString());
+                    if (digit % 2 != 0)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+
+            int sCounter = (from s in sNum where Char.IsNumber(s) && int.Parse(s.ToString()) % 2 != 0 select s).Count();
+
+            Console.WriteLine(sCounter);
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ Lab_7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab_7
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] cline = new char[] { 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '7' };
+            string sline = "Hello world7";
+
+            for (int i = 0; i < cline.Length; i++)
+            {
+                if (Char.IsNumber(cline[i]))
+                {
+                    cline[i] = '!';
+                }
+            }
+
+            for (int i = 0; i < cline.Length; i++)
+            {
+                Console.Write(cline[i]);
+            }
+            Console.WriteLine();
+
+            sline = sline.Replace('0', '!').Replace('1', '!').Replace('2', '!').Replace('3', '!').Replace('4', '!').Replace('5', '!').Replace('6', '!').Replace('7', '!').Replace('8', '!').Replace('9', '!');
+            Console.WriteLine(sline);
+            Console.ReadLine();
+        }
+    }
+}
